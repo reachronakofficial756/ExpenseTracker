4f5047d242b938276ee245d46f74ab699c607a56
Ronak Jain <reachronakofficial@gmail.com>
2026-01-31 13:23:14 +0530

    Refactor transaction handling and update form to require inputs

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,6 @@
 class Transaction {
-  constructor(type, amount, category, date, description) {
-    this.type = type; 
+  constructor(income, description, amount, category, date) {
+    this.income = income;
     this.amount = amount;
     this.category = category;
     this.date = date;
@@ -8,9 +8,21 @@ class Transaction {
   }
 }
 
-class ExpenseTracker {
-  constructor() {
-    this.transactions = [];
-  }
-}  
+let expenses = [];  
 
+function addTransaction(event) {
+  event.preventDefault();   
+    const income = Number(document.querySelector('input[name="income"]').value);
+    const description = document.querySelector('input[placeholder="Enter your description"]').value;
+    const amount = Number(document.querySelector('input[placeholder="0.00"]').value);
+    const category = document.getElementById("type").value;
+    const date = document.querySelector('input[type="date"]').value;
+    console.log(income, description, amount, category, date);    
+
+    
+
+    const transaction = new Transaction(income, description, amount, category, date);
+    expenses.push(transaction);
+
+    updateExpenseDisplay();
+}
